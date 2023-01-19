402014da5781d8b31bffe980398c1a0f2c27518d
dprithvi <113277815+dprithvi@users.noreply.github.com>
2023-01-19 22:32:12 +0530

    Time: 1 ms (99.38%), Space: 40.6 MB (55.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        if ((s.length() & 1) == 1) return false;
-  else {
-    Deque<Character> p = new ArrayDeque<>(s.length());
-    for (int i = 0; i < s.length(); i++)
-      switch (s.charAt(i)) {
-        case '(': p.push(')'); break;
-        case '{': p.push('}'); break;
-        case '[': p.push(']'); break;
-        case ')': case '}': case ']': if (p.isEmpty() || p.pop() != s.charAt(i)) return false;
-      }
-    return p.isEmpty();
-  }
-        
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
    
     
 }
